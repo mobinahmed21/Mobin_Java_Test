55e7f6f3066c0e10d9ef419f2f90def5bc1fafe5
Mobin <mobinahmed21@yahoo.com>
2017-07-03 06:51:10 +0530

    First Test Program

---
+++ TestJavaProj/src/com/home/TestJavaStringBuffer.java
@@ -0,0 +1,15 @@
+package com.home;
+
+public class TestJavaStringBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer();
+		sb.append("HELLO ");
+		sb.append("HOW ARE YOU ");
+		sb.append("MOBIN?");
+		System.out.println("OUTPUT IS:: "+sb.toString());
+
+	}
+
+}
