aa2ad79d959ce89dbdc2093c8dbd23c5532113e4
Mobin <mobinahmed21@yahoo.com>
2017-07-03 06:29:34 +0530

    My first git submission

---
+++ TestJavaProj/src/com/home/TestJavaCode.java
@@ -0,0 +1,11 @@
+package com.home;
+
+public class TestJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO MOBIN! HOW ARE YOU");
+
+	}
+
+}
